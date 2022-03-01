60a8eae0c95bd5346ce2157fb4e72aa25eecaafe
georgemac510 <georgemac510@gmail.com>
2022-03-01 18:46:52 -0500

    Adding Chainlink price feed

---
+++ scripts/deploy-chainlink.js
@@ -0,0 +1,30 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const PriceConsumerV3 = await hre.ethers.getContractFactory("PriceConsumerV3");
+  const priceConsumerV3 = await PriceConsumerV3.deploy();
+
+  await priceConsumerV3.deployed();
+
+  console.log("PriceConsumerV3 deployed to:", priceConsumerV3.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
